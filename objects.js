c7cef90ba65bb40616015140f0a53f11bec77a6f
bennu <benarzee1212@gmail.com>
2021-10-28 08:34:55 +0530

    new

---
+++ objects.js
@@ -0,0 +1,5 @@
+var person = {
+    firstName:"ben", lastName:"ben", age:25 , eyeColor:"black"
+};
+
+document.getElementById("demo").innerHTML = person.firstName + " is " + person.age + " years old.";
